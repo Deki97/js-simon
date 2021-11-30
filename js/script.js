bb4bb6d4e738539b76a9f7825f1a61dd0486163f
Deki97 <dejan97@libero.it>
2021-11-30 16:07:33 +0100

    prompt done

--- js/script.js
+++ js/script.js
@@ -3,3 +3,39 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+
+// Visualizzare con degli alert 5 numeri casuali. Da lì parte un timer di 30 secondi.
+let i = 0;
+const randomNumber = [];
+while(i < 5) {
+    let randomNum = getRndInteger(1, 100);
+    alert(randomNum);
+    randomNumber.push(randomNum);
+    i++;
+    // console.log(randomNum);
+}
+console.log('Numeri generati: ' + randomNumber);
+
+
+setTimeout(userInput, 3000);
+
+const userNumber = [];
+function userInput(number) {
+    let i = 0;
+    while(i < 5) {
+        number = parseInt( prompt('Inserisci un numero') );
+        // console.log(number);
+        i++;
+        userNumber.push(number);
+    }
+
+    console.log('Numeri ricordati: ' + userNumber);
+    
+}
+
+
+
+// Funzione che genera i numeri casuali da 1 a 100
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
